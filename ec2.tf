4cbe7a9303abc3522f23e3e42e3ffe1def82f08a
root <root@ip-172-31-23-79.eu-west-2.compute.internal>
2021-02-20 17:28:25 +0000

    ec2 instance class updated

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2" {
   ami           = "ami-098828924dc89ea4a"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name = "aws-ec2"
   availability_zone = "eu-west-2b"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id,aws_security_group.allow_ssh1.id] 
